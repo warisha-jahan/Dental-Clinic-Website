d49a1a7e9c848d44678d71a6c8396e94b54881da
warisha-jahan <officialwarishajahanzaib@gmail.com>
2024-08-01 10:25:33 +0500

    completed landing page

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('.hamburger').addEventListener('click', function() {
+    // Get the current state
+    const currentState = this.getAttribute('data-state');
+    const menu = document.querySelector('.menu'); // Select the menu element
+    const body = document.body; // Select the body element
+
+    // Toggle the state
+    if (currentState === 'closed') {
+        this.setAttribute('data-state', 'open');
+        this.classList.add('open');
+        menu.style.opacity= '1'; // Show the menu
+        body.style.overflow = 'hidden'; // Prevent body scrolling
+    } else {
+        this.setAttribute('data-state', 'closed');
+        this.classList.remove('open');
+        menu.style.opacity = '0'; // Hide the menu
+        body.style.overflow = ''; // Restore body scrolling
+    }
+});
